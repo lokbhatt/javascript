bbe6e1635ddb7cd267d37ba1aab17e3d6790882b
lokeshBhatt <bhattlok117@gmail.com>
2025-05-27 10:42:04 +0000

    comparision the operators

---
+++ 01_basics/comparision.js
@@ -0,0 +1,17 @@
+// console.log(2>1)
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2<=1);
+// console.log(2!=1);
+
+console.log(null > 0);
+console.log(null >= 0);
+// here >= comparision operator can convert null value into number 0.
+
+
+console.log(undefined == 0);
+
+
+// === strictly check the variable is datatype is same or not.
+// console.log("2" == 2); true
+// console.log("2" === 2); false.
