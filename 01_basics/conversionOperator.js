6eaeb954f927c425a70c36d489a45743c92d5de4
lokeshBhatt <bhattlok117@gmail.com>
2025-05-27 09:19:48 +0000

    ConversionOperators

---
+++ 01_basics/conversionOperator.js
@@ -0,0 +1,36 @@
+// let score = "33xya";
+// let score = undefined;
+
+// console.log(typeof score);
+
+// let strToInt = Number(score);
+
+// console.log(strToInt);
+
+// console.log(typeof strToInt);
+
+// notes
+// "33" => string
+// "33xya" => NaN
+// null => 0
+// undefind => NaN
+// true => 1; false => 0;
+
+let isLoggedIn ="Lokesh"
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(booleanLoggedIn);
+
+// notes
+// 1 => true
+// "" => false
+// "Lokesh" => true.
+
+let num= 45
+
+let numToStr = String(num)
+console.log(numToStr);
+console.log(typeof numToStr);
+
+// to convert number to String
+
+// *****************Operation*****************
