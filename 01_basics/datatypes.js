959c5bf86640b7e65f2a466eaadc6cc0c2dbd75d
lokeshBhatt <bhattlok117@gmail.com>
2025-05-27 08:43:33 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict" // treate all JS code as newer version.
+
+// alert(3+3); alert is not defined because i use Node js.
+let name = "lokesh" // string
+let age = 23; //integer
+let isActive = false //boolean
+let temperature = null
+
+// Number => 2^53
+// bigint 
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefind => 
+// symbol => uniqueness.
+
+// object
+
+console.log(typeof(123));
+console.log(typeof(null)); // output is object because it is object type.
