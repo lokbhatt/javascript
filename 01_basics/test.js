081cc59a2647971b5ca42dc36d2dc41750950fdb
lokeshBhatt <bhattlok117@gmail.com>
2025-05-27 07:52:47 +0000

    variables in js

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,16 @@
-console.log("lokesh");
+console.log("Start JavaScript PL.");
+
+// valiables is Js
+const accountId = 123456;
+let accountEmail = "lok@gamil.com";
+var accountPassword = 1234567;
+accountCity = "Kathamdu"; // must be avoid this type of variable declaration
+let dummy;
+// accountId = 123;  do not re assign the constant variable multiple times.
+
+accountEmail = "loks12@gmail.com"
+accountPassword = "54367890"
+/**
+ * in most cases to use let keyword because it have functional scope.
+ */
+console.table([accountId, accountEmail, accountPassword, accountCity, dummy]);
