75980fb077df06e48ea400072e3be49528b690cb
lokeshBhatt <bhattlok117@gmail.com>
2025-05-28 09:41:05 +0000

    numbers and maths

---
+++ 01_basics/numbers_and_maths.js
@@ -0,0 +1,23 @@
+const score = 400
+const balance = new Number(100)
+// console.log(balance);
+
+const number= 123.999
+// console.log(number.toPrecision(4)); //precise the value according to its size.
+// console.log(number.toString());
+
+const anotherNumber = 10000000000;
+// console.log(anotherNumber.toLocaleString('en-IN')); //en-IN can convert hindi or nepali format of number.
+
+
+
+/////////////////////******************Math*************////////////////////
+console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(5.2)); //it gives the value as nearby decimal value.
+// console.log(Math.ceil(5.2)); // if the value of variable is float then it converted into next integer variable.
+// console.log(Math.floor(5.9)); // it gives the floor value i.e. discard the floating point after the decimal.
+
+// console.log(Math.random()); // it can provide random number between 0 and 1
+console.log(Math.floor(Math.random() *10) + 1); //to generate random number differently.
+
